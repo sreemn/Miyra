2b7d701633b1e009f2663608757d18389f7d4014
Sreeman <110895150+sreemn@users.noreply.github.com>
2026-03-18 01:39:15 +0530

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -333,7 +333,7 @@ export default async function handler(req, res) {
         flags: 64,
         embeds: [
           {
-            color: 0x7e72ff,
+            color: 0x828ae2,
             description: `${username}'s Balance: ${user.balance.toLocaleString()} cookies 🍪`
           }
         ]
