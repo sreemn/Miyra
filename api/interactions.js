8ff6cbf3aee30b24b8b6ace1ed40ac97cae64c60
Sreeman <110895150+sreemn@users.noreply.github.com>
2026-03-18 01:41:13 +0530

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -380,7 +380,7 @@ export default async function handler(req, res) {
           flags: 64,
           embeds: [
             {
-              color: 0xfde8bb,
+              color: 0xf8eaba,
               description: `Bake again in ${formatTime(left)}`
             }
           ]
